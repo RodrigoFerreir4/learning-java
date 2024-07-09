95997627c9ed265bc004cef84b9f5c9829f19720
Rodrigo <rodrigo.4dev@gmail.com>
2024-07-09 00:23:55 -0400

    feat: crate CandidateEntity and integrate lombok lib

---
+++ jobs_api/src/main/java/com/jobs_api/modules/candidates/controllers/CandidateController.java
@@ -0,0 +1,18 @@
+package com.jobs_api.modules.candidates.controllers;
+
+import com.jobs_api.modules.candidates.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity){
+        System.out.println(candidateEntity.getEmail());
+
+    }
+}

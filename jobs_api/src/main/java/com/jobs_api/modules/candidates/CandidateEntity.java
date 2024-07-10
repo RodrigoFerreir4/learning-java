6174b87601d9f0dc0b4e1ea433f81418d38f5c4c
Rodrigo <rodrigo.4dev@gmail.com>
2024-07-10 00:56:42 -0400

    feat: add input validation

--- jobs_api/src/main/java/com/jobs_api/modules/candidates/CandidateEntity.java
+++ jobs_api/src/main/java/com/jobs_api/modules/candidates/CandidateEntity.java
@@ -1,18 +1,36 @@
 package com.jobs_api.modules.candidates;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 @Data
 public class CandidateEntity {
     private UUID id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    @Pattern(regexp = "^(?!\\s*$).+", message = "username não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo deve conter um email valido")
+    @NotNull
     private String email;
+
+    @NotNull
+    @Length(min = 5, max = 15)
     private String password;
+
     private String description;
+
     private String curriculum;
 }
 
